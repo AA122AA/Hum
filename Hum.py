a1af9337f188bc59d1a5e0e3303429cc58424920
Artem Gromov <66008477+AA122AA@users.noreply.github.com>
2020-06-18 11:54:22 +0300

    Update Hum.py

--- Hum.py
+++ Hum.py
@@ -4,12 +4,13 @@ import time
 
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(11,4)
-    print (type(time.ctime()))
+    
     t = str(temperature)
     h = str(humidity)
-    
+    print ("%s" % .format(time.ctime(), t, h))
     print ("temperature: %s C" % t)
     print ("humidity: %s %%" % h)
+    # you can write results to file
     #f = open("/home/pi/MagicMirror/modules/Hellloworld/test.txt", "w")
     #f.write("{:s}\ntemperature : {:s} C \nhumidity: {:s} %".format(time.ctime(), t, h))
     #f.flush()
